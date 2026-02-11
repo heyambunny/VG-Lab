fdb6c3ebecb17ba963f8f91e62ced0e2ae99b9f6
Bunny <harishmotwani@MacBook-Air.local>
2026-02-11 14:28:52 +0530

    app.py replaced with some test code

--- app.py
+++ app.py
@@ -1,68 +1,71 @@
-import streamlit as st
-from auth_config import ROLES
-from PIL import Image
+# import streamlit as st
+# from auth_config import ROLES
+# from PIL import Image
 
-# logo = Image.open("assets/vguard.png")
-# st.sidebar.image(logo, width=150)
+# # logo = Image.open("assets/vguard.png")
+# # st.sidebar.image(logo, width=150)
 
-st.set_page_config(
-    page_title="CSV Processing App",
-    layout="wide")
+# st.set_page_config(
+#     page_title="CSV Processing App",
+#     layout="wide")
 
-# -----------------------------
-# SESSION INITIALIZATION
-# -----------------------------
-if "authenticated" not in st.session_state:
-    st.session_state.authenticated = False
+# # -----------------------------
+# # SESSION INITIALIZATION
+# # -----------------------------
+# if "authenticated" not in st.session_state:
+#     st.session_state.authenticated = False
 
-if "role" not in st.session_state:
-    st.session_state.role = None
+# if "role" not in st.session_state:
+#     st.session_state.role = None
 
-# -----------------------------
-# GLOBAL AUTH GUARD
-# -----------------------------
-if not st.session_state.authenticated:
-    st.switch_page("pages/Login.py")
+# # -----------------------------
+# # GLOBAL AUTH GUARD
+# # -----------------------------
+# if not st.session_state.authenticated:
+#     st.switch_page("pages/Login.py")
 
-# -----------------------------
-# SIDEBAR (ROLE-BASED NAV)
-# -----------------------------
-st.sidebar.title("Navigation")
+# # -----------------------------
+# # SIDEBAR (ROLE-BASED NAV)
+# # -----------------------------
+# st.sidebar.title("Navigation")
 
-allowed_pages = ROLES[st.session_state.role]["allowed_pages"]
+# allowed_pages = ROLES[st.session_state.role]["allowed_pages"]
 
-PAGE_MAP = {
-    "index": "pages/index.py",
-    "Retailer Points": "pages/Retailer Points.py",
-    "msScheme": "pages/msScheme.py",
-    "Milestone": "pages/Milestone.py",
-    'Hubli Wires Scheme':"pages/Hubli Wires Scheme.py",
-    'SG Mega Bonanza': 'pages/SG Mega Bonanza.py',
-    'Ahmedabad Wires Scheme': 'pages/Ahmedabad Wires Scheme.py',
-    'data_import': 'pages/data_import.py',
-    "Sfdc Data Check": 'pages/Sfdc Data Check.py',
-    'Switch Gear Pan India': 'pages/Switch Gear Pan India.py',
-    'Hafele Sales Report':'Hafele Sales Report.py',
-    'eSeal':'eSeal.py'
-}
+# PAGE_MAP = {
+#     "index": "pages/index.py",
+#     "Retailer Points": "pages/Retailer Points.py",
+#     "msScheme": "pages/msScheme.py",
+#     "Milestone": "pages/Milestone.py",
+#     'Hubli Wires Scheme':"pages/Hubli Wires Scheme.py",
+#     'SG Mega Bonanza': 'pages/SG Mega Bonanza.py',
+#     'Ahmedabad Wires Scheme': 'pages/Ahmedabad Wires Scheme.py',
+#     'data_import': 'pages/data_import.py',
+#     "Sfdc Data Check": 'pages/Sfdc Data Check.py',
+#     'Switch Gear Pan India': 'pages/Switch Gear Pan India.py',
+#     'Hafele Sales Report':'Hafele Sales Report.py',
+#     'eSeal':'eSeal.py'
+# }
 
-for page in allowed_pages:
-    st.sidebar.page_link(PAGE_MAP[page], label=page)
+# for page in allowed_pages:
+#     st.sidebar.page_link(PAGE_MAP[page], label=page)
 
-st.sidebar.divider()
+# st.sidebar.divider()
 
-if st.sidebar.button("Logout"):
-    st.session_state.clear()
-    st.switch_page("pages/Login.py")
+# if st.sidebar.button("Logout"):
+#     st.session_state.clear()
+#     st.switch_page("pages/Login.py")
 
-# -----------------------------
-# HOME CONTENT
-# -----------------------------
-st.title("Dashboard")
+# # -----------------------------
+# # HOME CONTENT
+# # -----------------------------
+# st.title("Dashboard")
 
-st.success(f"Welcome, {st.session_state.role.upper()} user")
-st.write("Use the sidebar to navigate through the application.")
+# st.success(f"Welcome, {st.session_state.role.upper()} user")
+# st.write("Use the sidebar to navigate through the application.")
 
+#---------------------------------------------------------------------------------
+# CODE PUSHED
+#---------------------------------------------------------------------------------
 
 # tab1, tab2, tab3 = st.tabs(["Overview", "Branch-wise", "Member-wise"])
 
@@ -97,3 +100,9 @@ st.write("Use the sidebar to navigate through the application.")
 # st.metric("Error QRs", 250, delta="+45")
 
 # mode = st.toggle("Presentation Mode")
+
+import streamlit as st
+
+st.set_page_config(page_title="Test")
+
+st.title("App Running Successfully")
